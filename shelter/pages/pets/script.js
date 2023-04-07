b8c626ee97ca4b7365f8b2182347437941c949e3
Andrey <and.balyaeff@yandex.ru>
2023-04-07 20:07:50 +0700

    feat: I have added the pagination functionality taking into account the width of the screen

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-const pets =
+let pets =
 	[
 		{
 			"name": "Jennifer",
@@ -116,36 +116,6 @@ const pets =
 		}
 	]
 
-
-
-
-// const container = document.querySelector('.our-pets__container');
-
-// // Create and append a card element for each pet in the array
-// pets.forEach(pet => {
-// 	const card = document.createElement('div');
-// 	card.classList.add('our-pets__block');
-
-// 	const name = document.createElement('h2');
-// 	name.textContent = pet.name;
-// 	card.appendChild(name);
-
-// 	const img = document.createElement('img');
-// 	img.src = pet.img;
-// 	img.alt = pet.name;
-// 	card.appendChild(img);
-
-// 	container.appendChild(card);
-// });
-
-
-
-
-
-
-
-
-
 const container = document.querySelector('.our-pets__container');
 const firstPageBtn = document.querySelector('.first-page');
 const prevPageBtn = document.querySelector('.prev-page');
@@ -155,101 +125,122 @@ const lastPageBtn = document.querySelector('.last-page');
 
 const petsPerPage = 8;
 let currentPage = 1;
+let numPages;
+
+pets = shuffleArray(pets);
+
+const screenWidth = window.innerWidth;
+if (screenWidth >= 769) {
+	numPages = 6;
+} else if (screenWidth >= 321) {
+	numPages = 8;
+} else {
+	numPages = 16;
+}
+
+
+
 
 function generatePetCards(page) {
-  const startIndex = (page - 1) * petsPerPage;
-  const endIndex = Math.min(startIndex + petsPerPage, pets.length);
-  let pagePets = pets.slice(startIndex, endIndex);
+	const startIndex = (page - 1) * petsPerPage;
+	const endIndex = Math.min(startIndex + petsPerPage, pets.length);
+	let pagePets = pets.slice(startIndex, endIndex);
 
-  // тусуем карточки на др страницах
-  if (page > 1) {
-    pagePets = shuffleArray(pets.slice(0, endIndex)).slice(0, petsPerPage);
-  }
 
-  container.innerHTML = '';
+	if (page > 1) {
+		pagePets = shuffleArray(pets.slice(0, endIndex)).slice(0, petsPerPage);
+	}
 
-  pagePets.forEach(pet => {
-    const petCard = document.createElement('div');
-    petCard.classList.add('our-pets__block');
+	container.innerHTML = '';
 
-    const petImage = document.createElement('img');
-    petImage.src = pet.img;
-    petImage.alt = pet.name;
-    petCard.appendChild(petImage);
+	pagePets.forEach(pet => {
+		const petCard = document.createElement('div');
+		petCard.classList.add('our-pets__block');
 
-    const petName = document.createElement('p');
-    petName.innerText = pet.name;
-    petCard.appendChild(petName);
+		const petImage = document.createElement('img');
+		petImage.src = pet.img;
+		petImage.alt = pet.name;
+		petCard.appendChild(petImage);
 
-    container.appendChild(petCard);
-  });
+		const petName = document.createElement('p');
+		petName.innerText = pet.name;
+		petCard.appendChild(petName);
+
+		container.appendChild(petCard);
+	});
 }
 
 function shuffleArray(array) {
-  const shuffled = [...array];
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  }
-  return shuffled;
-}
+	const shuffled = [...array];
+	for (let i = shuffled.length - 1; i > 0; i--) {
+	  const j = Math.floor(Math.random() * (i + 1));
+	  const shouldSwap = Math.random() > 0.5; 
+	  if (shouldSwap) {
+		 [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+	  } else {
+		 const k = Math.floor(Math.random() * (i + 1));
+		 [shuffled[i], shuffled[k]] = [shuffled[k], shuffled[i]];
+	  }
+	}
+	return shuffled;
+ }
 
 function updateScoreboard() {
-  scoreboard.innerText = `Page ${currentPage}`;
+	scoreboard.innerText = currentPage;
 }
 
 function handleFirstPageClick() {
-  currentPage = 1;
-  generatePetCards(currentPage);
-  updateScoreboard();
-  firstPageBtn.disabled = true;
-  lastPageBtn.disabled = false;
+	currentPage = 1;
+	generatePetCards(currentPage);
+	updateScoreboard();
+	firstPageBtn.disabled = true;
+	lastPageBtn.disabled = false;
 }
 
 function handlePrevPageClick() {
-  if (currentPage > 1) {
-    currentPage--;
-    generatePetCards(currentPage);
-    updateScoreboard();
-    lastPageBtn.disabled = false;
-    if (currentPage === 1) {
-      firstPageBtn.disabled = true;
-    }
-  }
+	if (currentPage > 1) {
+		currentPage--;
+		generatePetCards(currentPage);
+		updateScoreboard();
+		lastPageBtn.disabled = false;
+		if (currentPage === 1) {
+			firstPageBtn.disabled = true;
+		}
+	}
 }
 
 function handleNextPageClick() {
-  if (currentPage < 6) {
-    currentPage++;
-    generatePetCards(currentPage);
-    updateScoreboard();
-    firstPageBtn.disabled = false;
-    if (currentPage === 6) {
-      lastPageBtn.disabled = true;
-    }
-  }
+	if (currentPage < numPages) {
+		currentPage++;
+		generatePetCards(currentPage);
+		updateScoreboard();
+		firstPageBtn.disabled = false;
+		if (currentPage === numPages) {
+			lastPageBtn.disabled = true;
+		}
+	}
 }
 
 function handleLastPageClick() {
-  currentPage = 6;
-  generatePetCards(currentPage);
-  updateScoreboard();
-  lastPageBtn.disabled = true;
-  firstPageBtn.disabled = false;
+	currentPage = numPages;
+	generatePetCards(currentPage);
+	updateScoreboard();
+	lastPageBtn.disabled = true;
+	firstPageBtn.disabled = false;
 }
 
-// создаем изначальные карточки петов и табло пагинации
+
 generatePetCards(currentPage);
 updateScoreboard();
 
-// слушатели событий по кнопкам
+
+firstPageBtn.disabled = true
+
+
 firstPageBtn.addEventListener('click', handleFirstPageClick);
 prevPageBtn.addEventListener('click', handlePrevPageClick);
 nextPageBtn.addEventListener('click', handleNextPageClick);
 lastPageBtn.addEventListener('click', handleLastPageClick);
-
-// отключаем кнопку - "в начало" на первой стр
-firstPageBtn.disabled = true;
 
 
 
