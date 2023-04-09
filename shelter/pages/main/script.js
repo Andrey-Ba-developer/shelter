edf0ad594dd95feac066e6309859a796a31e29d0
Andrey <and.balyaeff@yandex.ru>
2023-04-09 13:22:22 +0700

    feat: added functionality - sliders move from side to side

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -41,14 +41,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-
-
-
-
-
-
-
-
 // const modals = {
 // 	'katrine': document.getElementById('katrine'),
 // 	'jennifer': document.getElementById('jennifer'),
@@ -92,7 +84,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-const container = document.querySelector('.our-friends-content');
+
 
 
 let pets =
@@ -133,6 +125,9 @@ let pets =
 
 
 
+
+const container = document.querySelector('.our-friends-content');
+
 const prevPageBtn = document.querySelector('.our-friends__icon-block-left');
 const nextPageBtn = document.querySelector('.our-friends__icon-block-right');
 
@@ -146,66 +141,79 @@ pets = shuffleArray(pets);
 
 const petGroups = [];
 for (let i = 0; i < pets.length; i += 3) {
-  petGroups.push(pets.slice(i, i + 3));
+	petGroups.push(pets.slice(i, i + 3));
 }
 
 let currentGroupIndex = 0;
 
 let lastThreePets = [];
 
+
+
+
+
 function generatePetCards() {
+	let newHtml = '';
+	
+	// Удаляем все дочерние элементы контейнера
+	while (container.firstChild) {
+	  container.removeChild(container.firstChild);
+	}
+ 
+	
 	container.classList.add('our-friends-content-transition');
+ 
+	const currentGroup = petGroups[currentGroupIndex];
+ 
+	const filteredGroup = currentGroup.filter(pet => !lastThreePets.includes(pet));
+ 
+	let pagePets = filteredGroup;
+	while (pagePets.length < 3 && currentGroupIndex < petGroups.length - 1) {
+	  currentGroupIndex++;
+	  const nextGroup = petGroups[currentGroupIndex];
+	  const filteredNextGroup = nextGroup.filter(pet => !lastThreePets.includes(pet));
+	  pagePets = pagePets.concat(filteredNextGroup.slice(0, 3 - pagePets.length));
+	}
+ 
+	while (pagePets.length < 3 && currentGroupIndex > 0) {
+	  currentGroupIndex--;
+	  const prevGroup = petGroups[currentGroupIndex];
+	  const filteredPrevGroup = prevGroup.filter(pet => !lastThreePets.includes(pet));
+	  pagePets = filteredPrevGroup.slice(filteredPrevGroup.length - (3 - pagePets.length)).concat(pagePets);
+	}
+ 
+	lastThreePets = pagePets.slice(pagePets.length - 3);
+ 
+	container.innerHTML = '';
 
-  const currentGroup = petGroups[currentGroupIndex];
-
-  const filteredGroup = currentGroup.filter(pet => !lastThreePets.includes(pet));
-
-  let pagePets = filteredGroup;
-  while (pagePets.length < 3 && currentGroupIndex < petGroups.length - 1) {
-    currentGroupIndex++;
-    const nextGroup = petGroups[currentGroupIndex];
-    const filteredNextGroup = nextGroup.filter(pet => !lastThreePets.includes(pet));
-    pagePets = pagePets.concat(filteredNextGroup.slice(0, 3 - pagePets.length));
-  }
-
-  while (pagePets.length < 3 && currentGroupIndex > 0) {
-    currentGroupIndex--;
-    const prevGroup = petGroups[currentGroupIndex];
-    const filteredPrevGroup = prevGroup.filter(pet => !lastThreePets.includes(pet));
-    pagePets = filteredPrevGroup.slice(filteredPrevGroup.length - (3 - pagePets.length)).concat(pagePets);
-  }
-
-  lastThreePets = pagePets.slice(pagePets.length - 3);
-
-  container.innerHTML = '';
-
-	let cardId = 1;
-
-	pagePets.forEach(pet => {
-		const petCard = document.createElement('div');
-		petCard.classList.add('our-pets__block');
-		petCard.id = pet.name;
-
-		const petImage = document.createElement('img');
-		petImage.src = pet.img;
-		petImage.alt = pet.name;
-		petCard.appendChild(petImage);
-
-		const petName = document.createElement('p');
-		petName.innerText = pet.name;
-		petCard.appendChild(petName);
-
-		const learnMoreBtn = document.createElement('button');
-		learnMoreBtn.innerText = 'Learn More';
-		petCard.appendChild(learnMoreBtn);
-
-		container.appendChild(petCard);
+	pagePets.forEach((pet, index) => {
+		if (index >= 3) {
+		  newHtml += '<div class="our-pets__block hide">';
+		} else {
+		  newHtml += '<div class="our-pets__block">';
+		}
+		newHtml += `
+		  <img src="${pet.img}" alt="${pet.name}">
+		  <p>${pet.name}</p>
+		  <button>Learn More</button>
+		</div>
+		`;
+	 });
+ 
+	container.insertAdjacentHTML('beforeend', newHtml);
+ 
+	const cards = document.querySelectorAll('.our-pets__block');
+	cards.forEach((card, index) => {
+	  setTimeout(() => {
+		 if (index < 3) {
+			card.classList.add('show');
+		 } else {
+			card.classList.add('hide');
+		 }
+	  }, 100 * index);
 	});
-	setTimeout(() => {
-		container.classList.remove('our-friends-content-transition');
-	 }, 500);
-}
-
+ }
+ 
 
 
 
@@ -228,18 +236,42 @@ function shuffleArray(array) {
 
 
 
-
-
 function handlePrevPageClick() {
-		generatePetCards(currentPage);
-	}
+	container.classList.add('slide-animation');
+	container.style.transform = 'translateX(100%)';
+	setTimeout(() => {
+		 generatePetCards();
+		 container.classList.remove('slide-animation');
+		 container.style.transform = 'translateX(-100%)';
+		 setTimeout(() => {
+			  container.classList.add('slide-animation');
+			  container.style.transform = 'translateX(0%)';
+		 }, 0);
+	}, 500);
+}
+
 
 function handleNextPageClick() {
-		generatePetCards(currentPage);
-		}
+	container.classList.add('slide-animation');
+	container.style.transform = 'translateX(-100%)';
+	setTimeout(() => {
+		 generatePetCards();
+		 container.classList.remove('slide-animation');
+		 container.style.transform = 'translateX(100%)';
+		 setTimeout(() => {
+			  container.classList.add('slide-animation');
+			  container.style.transform = 'translateX(0%)';
+		 }, 0);
+	}, 500);
+}
 
 
-		
+
+
+
+
+
+
 generatePetCards(currentPage);
 
 prevPageBtn.addEventListener('click', handlePrevPageClick);
